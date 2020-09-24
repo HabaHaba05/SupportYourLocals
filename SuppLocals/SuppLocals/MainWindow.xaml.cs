1f2cb7be1478bc2cd2d77eac72d0334be5ab8674
unknown <lukasstc223@gmail.com>
2020-09-24 20:03:03 +0300

    Change menu to other file. Also, testing github

--- SuppLocals/SuppLocals/MainWindow.xaml.cs
+++ SuppLocals/SuppLocals/MainWindow.xaml.cs
@@ -24,8 +24,6 @@ namespace SuppLocals
     /// 
      
 
-    
-
     public partial class MainWindow : Window
     {
         
@@ -86,80 +84,6 @@ namespace SuppLocals
             filterServiceTypeCB.ItemsSource = types;
             filterServiceTypeCB.SelectedIndex = 0;
         }
-
-        
-
-        private void buttonClick(object sender, RoutedEventArgs e)
-        {
-            if (sPan.Visibility == Visibility.Collapsed)
-            {
-                sPan.Visibility = Visibility.Visible;
-                (sender as Button).Content = "☰";
-            }
-            else
-            {
-                sPan.Visibility = Visibility.Collapsed;
-                (sender as Button).Content = "☰";
-            }
-        }
-
-        private void servicesClick(object sender, RoutedEventArgs e)
-        {
-            if (servicePanel.Visibility == Visibility.Collapsed)
-            {
-                servicePanel.Visibility = Visibility.Visible;
-                (sender as Button).Content = "Vendors";
-            }
-            else
-            {
-                servicePanel.Visibility = Visibility.Collapsed;
-                (sender as Button).Content = "Vendors";
-            }
-        }
-
-        private void filterClick(object sender, RoutedEventArgs e)
-        {
-            if (filterPanel.Visibility == Visibility.Collapsed)
-            {
-                filterPanel.Visibility = Visibility.Visible;
-                (sender as Button).Content = "Filters";
-            }
-            else
-            {
-                filterPanel.Visibility = Visibility.Collapsed;
-                (sender as Button).Content = "Filters";
-            }
-        }
-
-        private void reviewsClick(object sender, RoutedEventArgs e)
-        {
-            if (reviewsPanel.Visibility == Visibility.Collapsed)
-            {
-                reviewsPanel.Visibility = Visibility.Visible;
-                (sender as Button).Content = "Reviews";
-            }
-            else
-            {
-                reviewsPanel.Visibility = Visibility.Collapsed;
-                (sender as Button).Content = "Reviews";
-            }
-        }
-
-        private void MoreButtonClicked(object sender, RoutedEventArgs e)
-        {
-            ReviewsWindow rWindow = new ReviewsWindow();
-            rWindow.Show();
-        }
-
-
-        private void hyperlinkRequestNavigate(object sender, RequestNavigateEventArgs e)
-        {
-            //true if the shell should be used when starting the process; false if the process should be created directly from the executable file.
-            //The default is true on.NET Framework apps and false on.NET Core apps.
-            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri) { UseShellExecute = true });
-            e.Handled = true;
-        }
-
 
         public void drawCircle(Microsoft.Maps.MapControl.WPF.Location Loc, double dRadius, Color fillColor)
         {
---
+++ SuppLocals/SuppLocals/Menu.cs
@@ -0,0 +1,83 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+
+namespace SuppLocals
+{
+    public partial class MainWindow
+    {
+        /* There is methods for Menu*/
+        private void buttonClick(object sender, RoutedEventArgs e)
+        {
+            if (sPan.Visibility == Visibility.Collapsed)
+            {
+                sPan.Visibility = Visibility.Visible;
+                (sender as Button).Content = "☰";
+            }
+            else
+            {
+                sPan.Visibility = Visibility.Collapsed;
+                (sender as Button).Content = "☰";
+            }
+        }
+
+        private void servicesClick(object sender, RoutedEventArgs e)
+        {
+            if (servicePanel.Visibility == Visibility.Collapsed)
+            {
+                servicePanel.Visibility = Visibility.Visible;
+                (sender as Button).Content = "Vendors";
+            }
+            else
+            {
+                servicePanel.Visibility = Visibility.Collapsed;
+                (sender as Button).Content = "Vendors";
+            }
+        }
+
+        private void filterClick(object sender, RoutedEventArgs e)
+        {
+            if (filterPanel.Visibility == Visibility.Collapsed)
+            {
+                filterPanel.Visibility = Visibility.Visible;
+                (sender as Button).Content = "Filters";
+            }
+            else
+            {
+                filterPanel.Visibility = Visibility.Collapsed;
+                (sender as Button).Content = "Filters";
+            }
+        }
+
+        private void reviewsClick(object sender, RoutedEventArgs e)
+        {
+            if (reviewsPanel.Visibility == Visibility.Collapsed)
+            {
+                reviewsPanel.Visibility = Visibility.Visible;
+                (sender as Button).Content = "Reviews";
+            }
+            else
+            {
+                reviewsPanel.Visibility = Visibility.Collapsed;
+                (sender as Button).Content = "Reviews";
+            }
+        }
+
+        private void MoreButtonClicked(object sender, RoutedEventArgs e)
+        {
+            ReviewsWindow rWindow = new ReviewsWindow();
+            rWindow.Show();
+        }
+
+
+        private void hyperlinkRequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            //true if the shell should be used when starting the process; false if the process should be created directly from the executable file.
+            //The default is true on.NET Framework apps and false on.NET Core apps.
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri) { UseShellExecute = true });
+            e.Handled = true;
+        }
+
+    }
+}
