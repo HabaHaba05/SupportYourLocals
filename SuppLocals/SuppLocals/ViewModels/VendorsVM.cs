addac086c5957503739f9497094c91ccf624208c
Bertašius Paulius <paulius.bertasius@mif.stud.vu.lt>
2020-10-24 16:45:46 +0300

    Using OnPrepertyChanged with generics also optional argument. (6th and 3rd requirments fulfilled.)

--- SuppLocals/SuppLocals/ViewModels/VendorsVM.cs
+++ SuppLocals/SuppLocals/ViewModels/VendorsVM.cs
@@ -3,22 +3,30 @@ using System.Linq;
 
 namespace SuppLocals.ViewModels
 {
-    public class VendorsVM : BaseViewModel
+    public class VendorsVM : ObservableObject
     {
         public VendorsVM()
         {
-            UserList = new List<User>();
+            _userList = new List<User>();
 
             using (var db = new UsersDbTable())
             {
                 var data = db.Users.ToList();
                 foreach (var user in data.Where(user => user.VendorsCount > 0))
                 {
-                    UserList.Add(user);
+                    _userList.Add(user);
                 }
             }
         }
 
-        public List<User> UserList { get; }
+        private List<User> _userList;
+        public List<User> UserList 
+        { 
+            get => _userList;
+            set
+            {
+                OnPropertyChanged(ref _userList, value);
+            }
+        }
     }
 }
