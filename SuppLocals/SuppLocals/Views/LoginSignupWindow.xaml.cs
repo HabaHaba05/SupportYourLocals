7b0bedc3396f92926bb435fac059e5babe726deb
unknown <lukasstc223@gmail.com>
2020-10-26 15:07:05 +0200

    MVVM login hell

---
+++ SuppLocals/SuppLocals/Views/LoginSignupWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using SuppLocals.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace SuppLocals.Views
+{
+    public partial class LoginSignupWindow : Window
+    {
+        public LoginSignupWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DataContext = new LoginVM();
+        }
+    }
+}
